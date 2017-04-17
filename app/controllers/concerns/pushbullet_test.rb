b8025fbd5c82e2f9cbf34877b5dbfa475bc01e7d
Coda <sopurani885@gmail.com>
2017-04-17 19:38:59 +0900

    modularized

--- app/controllers/concerns/pushbullet_test.rb
+++ app/controllers/concerns/pushbullet_test.rb
@@ -1,14 +1,30 @@
 require 'washbullet'
 #require 'dotenv-rails'
+require "dotenv"
 
+module PushbulletTest
+  extend ActiveSupport::Concern
 
-#Dotenv.load ".env"
+  def init
+    Dotenv.load ".env"
 
-p ENV['TOKEN']
+    p ENV['TOKEN']
 
-client = Washbullet::Client.new(ENV['TOKEN'])
+#    client = Washbullet::Client.new(ENV['TOKEN'])
 
-  client.push_note(receiver: :device, identifier:ENV['DEVICE'], params:{
-      title: '朝やで',
-      body: '朝'
-  })
+#    client.push_note(receiver: :device, identifier:ENV['DEVICE'], params:{
+#        title: '朝やで',
+#        body: '朝'
+#    })
+
+  end
+
+  def push
+    @user = User.all
+    puts "user~~~~~~~~~~~~~~~~~~~~~~"
+    @user.each do |user|
+      p user
+    end
+  end
+
+end
