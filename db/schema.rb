dfcabd4d8f5a40b1361b6319f4a74ff2c1985138
Ranats <sopurani885@gmail.com>
2017-04-28 15:34:22 +0900

    for production

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -32,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170411110000) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.time     "pushtime",   default: '2000-01-01 06:00:00'
+    t.string   "title",      default: "今日のゴミ"
     t.datetime "created_at",                                 null: false
     t.datetime "updated_at",                                 null: false
   end
